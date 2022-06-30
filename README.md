f607a7fa77b4a6dd87d6720607b223c6b316b6cb
MahrezBH <mahrez.benhamad@gmail.com>
2022-06-30 18:25:47 +0100

    main commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Projet Challenge Team (2022/2023)
+
+Theme: timeline
+
+## Equipe
+
+* Tajouri Ahmed
+* Rim SELMI
+* Hanen NAOUA
+* Mbarek Haboubi
+* Adel thebti
+* Mahrez BENHAMAD
+
+
+
+## Encadrents
+* Mohamed Amine CHEBBI
+* Hechmi Mohamed Amine
+
+## Installation
+1) git clone https://github.com/MahrezBH/timeline_project.git
+2) entrer le projet (repo)
+3) npm i
---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose')
+
+var app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// database connection
+const dbURI = 'mongodb+srv://team:teampass@challenge-team.sg02r8r.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+  .then((result) => {
+    console.log("Datebase connect with success!");
+    app.listen(4000)
+  })
+  .catch((err) => {
+    console.log("Datebase connect with fail!");
+    console.log(err)
+  });
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
